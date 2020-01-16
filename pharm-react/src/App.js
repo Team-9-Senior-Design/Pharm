4f95e2e9f8ce8c5ddc425940a23b6878c4aa7ee3
William Otey <wko0001@auburn.edu>
2020-01-16 16:00:24 -0600

    new code

---
+++ pharm-react/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>
+            <img src="i.png" alt="top" width="120" height="120"/>
+        </div>
+        
+
+    </header>
+    <section>
+        <div id="main">
+            <div align="center" >
+                <img src="Unknown.jpg" alt="Trulli"/></div>
+            <div >
+                <h1 align="center">introduction</h1>
+                <p ><a href="url">go start</a></p>
+            </div>
+        </div>
+    </section>
+    </div>
+  );
+}
+
+export default App;
